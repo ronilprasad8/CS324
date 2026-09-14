91ff4f663462827f51edf68712bccc0d0c777ba0
ronilprasad8 <ronilprasad46@gmail.com>
2026-09-14 14:11:12 +1200

    Add gRPC Java Lab

---
+++ src/main/java/com/example/grpc/HelloWorldServer.java
@@ -0,0 +1,32 @@
+package com.example.grpc;
+
+/** 
+* 
+* @author ronilprasad8
+*/
+import com.example.grpc.HelloWorldProto.HelloReply;
+import com.example.grpc.HelloWorldProto.HelloRequest;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+
+public class HelloWorldServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server server = ServerBuilder.forPort(50051).addService(new GreeterImpl()).build().start();
+
+        System.out.println("Server started, listening on " + server.getPort());
+        server.awaitTermination();
+    }
+
+    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+        @Override
+        public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+    }
+}
